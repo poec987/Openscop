4d6a0cfa55dd684bf18ab72b54002bd2af5092bd
TechMan06 <132509305+TechMan06@users.noreply.github.com>
2023-09-30 19:22:51 -0300

    camera

--- scripts/global.gd
+++ scripts/global.gd
@@ -2,13 +2,16 @@ extends Node
 
 #ROOM
 var room_name ="test"
-var camera_limit_hor = 2
-var camera_limit_ver = 2
-var camera_rot = 2.6
-var camera_dist_hor = 2.6
-var camera_dist_ver = 2.6
-var camera_move_hor = true
-var camera_move_ver = true
+var camera_limit_x = 2
+var camera_limit_z = 2
+var camera_limit_y = 4
+var camera_rot = -18
+var camera_dist_hor = 14
+var camera_dist_ver = 4
+var camera_move_x = true
+var camera_move_z = true
+var camera_move_y = true
+var camera_root_dist_ver = 0.0
 
 #GAME
 var control_mode = 0
--- scripts/player/camera_root.gd
+++ scripts/player/camera_root.gd
@@ -1,11 +1,20 @@
 extends Node3D
 
-const CAMERA_SPEED = 3.0
-
+@onready var camera = get_node("player_camera")
 @onready var player = get_node("../player")
-
-func _process(delta):
-	if (abs(position.x-player.position.x) > Global.camera_limit_hor):
+func _ready():
+	camera.position.x = Global.camera_dist_hor
+	camera.position.y = Global.camera_dist_ver
+	
+func _process(delta):		
+	if (abs(position.x-player.position.x) > Global.camera_limit_x) && Global.camera_move_x:
 		position.x -= ((((position.x-player.position.x) / abs(position.x-player.position.x)))) * delta * abs(player.velocity.x)
-	if (abs(position.z-player.position.z) > Global.camera_limit_ver):
+	if (abs(position.z-player.position.z) > Global.camera_limit_z) && Global.camera_move_z:
 		position.z -= ((((position.z-player.position.z) / abs(position.z-player.position.z)))) * delta * abs(player.velocity.z)
+	if (abs(position.z-player.position.z) > Global.camera_limit_y) && Global.camera_move_y:
+		position.y -= ((((position.y-player.position.y) / abs(position.z-player.position.y)))) * delta * abs(player.velocity.y)
+		
+func _setup():
+	camera.position.x = Global.camera_dist_hor
+	camera.position.y = Global.camera_dist_ver
+	camera.rotation.x = Global.camera_rot
