b5ab0311b827011572dae885e2dc88d5232b3fc6
TechMan06 <132509305+TechMan06@users.noreply.github.com>
2023-10-06 21:00:08 -0300

    quick code fix

--- scripts/player/grass.gd
+++ scripts/player/grass.gd
@@ -5,7 +5,7 @@ extends Node3D
 const TILE_SIZE = 29
 var current_position = [0,0]
 
-func _process(delta):
+func _process(_delta):
 	position = Vector3(round(get_node("../../player").position.x/TILE_SIZE)*TILE_SIZE,0,round(get_node("../../player").position.z/TILE_SIZE)*TILE_SIZE)
 
 
