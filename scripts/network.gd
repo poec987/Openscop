57a94f70de8a46b1d612f6ab8c464a5f920b8c17
TechMan06 <132509305+TechMan06@users.noreply.github.com>
2023-10-07 20:53:40 -0300

    Networking

--- scripts/network.gd
+++ scripts/network.gd
@@ -1,11 +1,29 @@
 extends Node
 
+var port = 9999
+@rpc("any_peer")
+func _send_player_information(n_ame, id, score = 0):
+	if !GameManager.Players.has(id):
+		GameManager.Players[id] = {
+			"name" : n_ame,
+			"id" : id,
+			"score" : score
+		}
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	if multiplayer.is_server():
+		for i in GameManager.Players:
+			_send_player_information.rpc(GameManager.Players[i].name, i)
+			
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("host"):
+		var peer = ENetMultiplayerPeer. new()
+		var error = peer.create_server(port)
+		if error != OK:
+			print("Cannot host: " + error)
+			return
+		peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+
+		multiplayer.set_multiplayer_peer(peer)
+		print("Waiting for players")
+
+		_send_player_information("", multiplayer.get_unique_id())
