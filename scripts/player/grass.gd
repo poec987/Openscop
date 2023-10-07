be6ca48e99e865740bbab28f2f26bcade451bd13
TechMan06 <132509305+TechMan06@users.noreply.github.com>
2023-10-06 21:06:49 -0300

    Code shrinking

--- scripts/player/grass.gd
+++ scripts/player/grass.gd
@@ -1,11 +1,4 @@
 extends Node3D
-
-
-# Called when the node enters the scene tree for the first time.
 const TILE_SIZE = 29
-var current_position = [0,0]
-
 func _process(_delta):
 	position = Vector3(round(get_node("../../player").position.x/TILE_SIZE)*TILE_SIZE,0,round(get_node("../../player").position.z/TILE_SIZE)*TILE_SIZE)
-
-
