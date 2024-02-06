a27712d0a7633871fdf33e7e1aa0ce46b255f94b
TechMan06 <132509305+TechMan06@users.noreply.github.com>
2024-02-06 20:51:15 -0300

    pause menu finished

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -12,6 +12,7 @@ var unpause_anim = false
 var selected_option = 0
 var active_menu = [false,false,false,false,false]
 var spawned_menu = false
+var inMenu = false
 
 func get_screen():
 	var viewport_feed: Viewport =  get_tree().root.get_viewport()
@@ -29,21 +30,31 @@ func selection_sound(variable):
 
 
 func _process(_delta):
-
+	
+	if $current_menu.get_child_count()==0 && inMenu == true:
+		create_tween().tween_property(self, "fade", 0.0,0.5)
+		if fade<1.1:
+			create_tween().tween_property($pink_fade,"color:a",0.0,0.25)
+		await get_tree().create_timer(0.4).timeout
+		inMenu = false
+		active_menu = [false,false,false,false,false]
+		
 	if Input.is_action_just_pressed("pressed_start") && Global.control_mode==0 && get_tree().get_first_node_in_group("HUD_pausemenu").can_unpause:
 		if $current_menu.get_child_count()==0:
 			Global.game_paused=!Global.game_paused
+	var pets = get_node_or_null("pets")
 	
 	
 	if Global.game_paused:
 		get_tree().paused = true
+		
 		if $screen_sprite.scale.x>MINI_SCREEN_SIZE:
 			can_unpause=false
 			$screen_sprite.z_index=1
 		else:
 			can_unpause=true
 			$screen_sprite.z_index=-1
-			#$frame.visible=false
+			
 		if !screenshotted:
 			var random=randi_range(0,2)
 			if random==0:
@@ -54,10 +65,12 @@ func _process(_delta):
 				$pause_sound3.play()
 			$screen_sprite.set_texture(get_screen())
 			$main_pausemenu/background1.visible = true
-			$main_pausemenu/buttons.visible = true
+			if $main_pausemenu/buttons_quit.get_child_count()==0 && !inMenu:
+				$main_pausemenu/buttons.visible = true
 			screenshotted = true
 			create_tween().tween_property($screen_sprite,"scale",Vector2(MINI_SCREEN_SIZE,MINI_SCREEN_SIZE),SCREEN_ANIM_TIME).set_trans(Tween.TRANS_SINE)
 	else:
+		
 		if $screen_sprite.scale.x<1.:
 			can_unpause=false
 		else:
@@ -75,11 +88,13 @@ func _process(_delta):
 			$main_pausemenu/background1.visible = false
 			$main_pausemenu/buttons.visible = false
 			$screen_sprite.set_texture(null)
+		
 		if $screen_sprite.scale.x==MINI_SCREEN_SIZE:
 			$screen_sprite.z_index=1
 			create_tween().tween_property($screen_sprite,"scale",Vector2(1.,1.),SCREEN_ANIM_TIME).set_trans(Tween.TRANS_SINE)
 
-	if can_unpause && Global.game_paused && $current_menu.get_child_count()==0:
+
+	if can_unpause && Global.game_paused && $current_menu.get_child_count()==0 && $main_pausemenu/buttons.visible:
 		if fade==0.0:
 			if Input.is_action_just_pressed("pressed_up") && selected_option!=0:
 				selected_option-=1
@@ -90,15 +105,21 @@ func _process(_delta):
 				selection_sound(selected_option)
 		
 		$overlay.set_modulate(Color(1,1,1,fade))
-		if fade>1.0:
+		
+		if fade>1.0 && inMenu == false:
 			create_tween().tween_property($pink_fade,"color:a",1.0,0.25)
 			if $pink_fade.color.a>0.9 && active_menu[selected_option]==false:
 				if selected_option==2:
 					$current_menu.add_child(preload("res://scenes/objects/menu/pets.tscn").instantiate())
 					active_menu[selected_option]=true
+					inMenu = true
 
 		if Input.is_action_just_pressed("pressed_action"):
-			if selected_option!=0:
+			if selected_option==4:
+				#$selected.play()
+				active_menu[selected_option]=true
+				$current_menu.add_child(preload("res://scenes/objects/menu/quit_button.tscn").instantiate())
+			elif selected_option!=0:
 				if fade==0.0 && $current_menu.get_child_count()==0:
 					$selected.play()
 				$overlay.frame_coords.x=selected_option
