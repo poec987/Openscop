331a0b9e353871016ef76b359bd8ca508b49463a
izz <zoomer589@gmail.com>
2024-02-27 13:59:05 -0500

    code cleanup a bit

--- scripts/ui/console.gd
+++ scripts/ui/console.gd
@@ -1,14 +1,13 @@
 extends Control
 
+# WIP!
 @export_subgroup("Console Setup")
 @export var console_input : TextEdit
 @export var console_output : RichTextLabel
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
