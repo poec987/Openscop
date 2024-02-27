42662b13007e711edcdadd1cdb3389500b1dfd12
izz <zoomer589@gmail.com>
2024-02-27 14:19:13 -0500

    documentation for console

--- scripts/ui/console.gd
+++ scripts/ui/console.gd
@@ -1,10 +1,11 @@
 extends Control
 
-# WIP!
-@export_subgroup("Console Setup")
-@export var console_input : TextEdit
-@export var console_output : RichTextLabel
 
+@export_category("Console")
+## The [TextEdit] to be parsed.
+@export var console_input : TextEdit
+## The [RichTextLabel] to display Console output.
+@export var console_output : RichTextLabel
 
 func _ready():
 	pass 
