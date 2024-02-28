f0a3cb7fa2fb4e901641a93f7ca6fad11c11949a
izzint <zoomer589@gmail.com>
2024-02-28 09:22:31 -0500

    this works for now

--- scripts/ui/console.gd
+++ scripts/ui/console.gd
@@ -7,20 +7,23 @@ extends Window
 ## The [RichTextLabel] to display Console output.
 @export var console_output : RichTextLabel
 
+func _parse_command(input : Array):
+	match input[0]:
+		"Test":
+			console_log("test")
+		_:
+			console_log("Invalid Command")
+		
+			
+func console_log(input):
+	console_output.append_text(input)
+	console_output.newline()
+	console_input.clear()
+
 func _close_requested():
 	visible = false
 
 func _on_input_text_submitted(new_text):
+	# Create a command array using the input text 
 	var command_array = new_text.split(" ")
 	_parse_command(command_array)
-	
-func _parse_command(input : Array):
-	match input[0]:
-		"Test":
-			_log("test")
-		"_":
-			print("INVALID COMMAND!")
-			
-func _log(input):
-	console_output.append_text(input)
-	console_input.clear()
