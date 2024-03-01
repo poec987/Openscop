999481d2610fe6b00de880e13f21ff5375f4e57b
TechMan06 <132509305+TechMan06@users.noreply.github.com>
2024-03-01 12:26:56 -0300

    bucket semi-implemented

--- scripts/objects/level_objects/bucket.gd
+++ scripts/objects/level_objects/bucket.gd
@@ -10,5 +10,7 @@ func _physics_process(delta):
 	h = int($raycast/x_positive.is_colliding() && $raycast/x_positive.get_collider(0)==get_tree().get_first_node_in_group("Player"))*-1 + int($raycast/x_negative.is_colliding() && $raycast/x_negative.get_collider(0)==get_tree().get_first_node_in_group("Player"))
 	velocity.z = lerp(velocity.z,v*float(get_tree().get_first_node_in_group("Player").movement_speed),(delta)*ACCELERATION)
 	velocity.x = lerp(velocity.x,h*float(get_tree().get_first_node_in_group("Player").movement_speed),(delta)*ACCELERATION)
-	
+	#if global_position.distance_to(get_tree().get_first_node_in_group("Player").position)<0.5:
+		#velocity.z = lerp(velocity.z,v*float(get_tree().get_first_node_in_group("Player").movement_speed*4),(delta)*ACCELERATION)
+		#velocity.x = lerp(velocity.x,h*float(get_tree().get_first_node_in_group("Player").movement_speed*4),(delta)*ACCELERATION)
 	move_and_slide()
