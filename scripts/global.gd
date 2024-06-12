b7b702917670f5ac8f4acdd512a91194fe1624f7
TechMan06 <132509305+TechMan06@users.noreply.github.com>
2024-06-12 10:14:25 -0300

    finalized showcase

--- scripts/global.gd
+++ scripts/global.gd
@@ -55,7 +55,7 @@ var piece_log = {
 
 var player_array = Vector4(0.,0.,0.,0)
 var player_brightness = 1.0
-var pets = [false,false,false,false,false,false,true,false,false,false]
+var pets = [false,false,false,false,false,false,false,false,false,false]
 var fog_focus = 0
 #0 = follow player
 #
