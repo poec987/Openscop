1f1b47f1c8a4cdb28bfd9af516fa0349abc51588
TechMan06 <132509305+TechMan06@users.noreply.github.com>
2024-08-04 20:41:42 -0300

    incomplete camera

--- scripts/objects/camera_zone.gd
+++ scripts/objects/camera_zone.gd
@@ -2,8 +2,6 @@
 extends Node3D
 
 @export_category("Properties")
-@export var place_camera = false
-@export var rotate_camera = false
 @export var instant = false
 @export var use_default_smooth = true
 @export var smoothness = 1.0
@@ -14,57 +12,38 @@ extends Node3D
 
 @onready var player = get_tree().get_first_node_in_group("Player")
 
+@onready var target_position = Vector3.ZERO 
 var inside = false
+func _ready():
+	target_position = get_child(1).global_position
+
 func _process(delta):
 	$zone_area/zone_collision.get_shape().size= scale
 	$zone_area/zone_collision/zone_mesh.scale = scale
 	$zone_area/zone_collision.get_shape().size = scale
+	
 	if !Engine.is_editor_hint():
+		get_tree().get_first_node_in_group("Player_camera").pos_argument = get_tree().get_first_node_in_group("Player_camera").global_position
 		visible = Global.debug
 		if inside:
-			if place_camera:
-				if use_default_smooth:
-					smoothness = 1.0
-				if !instant:
-					get_tree().get_first_node_in_group("Player_camera").pos_argument = get_tree().get_first_node_in_group("Player_camera").pos_argument.lerp(get_child(1).global_position, smoothness*delta)
-					if rotate_camera:
-						get_tree().get_first_node_in_group("Player_camera").rot_argument = get_tree().get_first_node_in_group("Player_camera").rot_argument.lerp(get_child(1).global_rotation, smoothness*delta)
-					else:
-						get_tree().get_first_node_in_group("Player_camera").rot_argument = null
-				else:
-					get_tree().get_first_node_in_group("Player_camera").pos_argument = get_child(1).global_position
-					if rotate_camera:
-						get_tree().get_first_node_in_group("Player_camera").rot_argument = get_child(1).global_rotation
-					else:
-						get_tree().get_first_node_in_group("Player_camera").rot_argument = null
-	else:
-		if place_camera && get_child_count()<2:
-			print("ADD NODE AS A CHILD TO THIS OBJECT FIRST!!")
-			place_camera = false
+			if use_default_smooth:
+				smoothness = 1.0
+			if !instant:
+				get_tree().get_first_node_in_group("Player_camera").pos_argument = get_tree().get_first_node_in_group("Player_camera").global_position.lerp(target_position, smoothness*delta)
+			else:
+				get_tree().get_first_node_in_group("Player_camera").pos_argument = target_position
 		else:
-			if get_node_or_null("Camera3D"):
-				get_child(1).fov = 35.0
-
+			if use_default_smooth_out:
+				smoothness = 1.0
+			if !instant:
+				get_tree().get_first_node_in_group("Player_camera").pos_argument = get_tree().get_first_node_in_group("Player_camera").pos_argument.lerp(get_tree().get_first_node_in_group("Player").global_position, smoothness*delta)
+			else:
+				get_tree().get_first_node_in_group("Player_camera").pos_argument = target_position
+				
 func _on_zone_area_body_entered(body):
 	if body==get_tree().get_first_node_in_group("Player"):
 		inside = true
-		if place_camera:
-			get_tree().get_first_node_in_group("Player_camera").pos_argument = get_tree().get_first_node_in_group("Player_camera").get_child(0).global_position
-			if rotate_camera:
-				get_tree().get_first_node_in_group("Player_camera").rot_argument = get_tree().get_first_node_in_group("Player_camera").get_child(0).global_rotation
-			Global.camera_mode = 1
+		Global.camera_mode = 1
 func _on_zone_area_body_exited(body):
 	if body==get_tree().get_first_node_in_group("Player"):
 		inside = false
-		
-		#TO BE FIXED
-		if use_default_smooth_out:
-			smoothness_out = 5.5
-		
-		get_tree().get_first_node_in_group("Player_camera").smooth_out = smoothness_out
-		
-		if instant_out:	
-			get_tree().get_first_node_in_group("Player_camera").smooth_out = -1.0
-		
-		if place_camera:
-			Global.camera_mode = 0
--- scripts/player/camera_root.gd
+++ scripts/player/camera_root.gd
@@ -5,9 +5,8 @@ const CAMERA_SPEED = 3.0
 @onready var cam = $cam_anchor/PSXLayer 
 @onready var player = get_node("../player")
 
-@export var pos_argument = Vector3.ZERO
-@export var rot_argument = Vector3.ZERO
-@export var smooth_out: float
+@onready var pos_argument = Vector3.ZERO
+
 func _setup():
 	#SETS UP CAMERA ACCORDING TO ROOM, SPECIFIED PROPERTIES
 	if Global.camera_mode==0:
@@ -26,11 +25,12 @@ func _setup():
 		position.y = Global.camera_freeze_y
 	if !Global.camera_move_z:
 		position.z = Global.camera_freeze_z
+		
+	if Global.camera_mode==1:
+		global_position = pos_argument
+	
 func _process(delta):
 	_setup()
-	
-	if rot_argument==null:
-		rot_argument = cam_anchor.global_rotation
 	
 	#CAMERA MODE 0 = FOLLOW PLAYER	
 	if Global.camera_mode==0:
@@ -44,23 +44,14 @@ func _process(delta):
 		if (abs(position.y-player.position.y) > Global.camera_limit_y) && Global.camera_move_y:
 			position.y -= ((((position.y-player.position.y) / abs(position.y-player.position.y)))) * delta * abs(player.velocity.y)
 		# set the pos and rot of the camera to follow the cam anchor
+
 	_setup()
 	
 	if Global.camera_mode==0:
-		if smooth_out==-1.0 || smooth_out==null:
-			if Global.control_mode<4:
-				cam.set_cam_pos(cam_anchor.global_position, cam_anchor.global_rotation)
-			elif Global.control_mode>=4 && Global.control_mode<=5:
-				cam.set_cam_pos(Vector3(get_tree().get_first_node_in_group("Player").global_position.x,1,get_tree().get_first_node_in_group("Player").global_position.z), Vector3(0.,-1.5708+get_tree().get_first_node_in_group("Player").rotation.y,0.))
-		else:
-			cam.set_cam_pos(pos_argument, rot_argument)
-			pos_argument = pos_argument.lerp(cam_anchor.global_position,smooth_out*delta)
-			rot_argument = rot_argument.lerp(cam_anchor.global_rotation,smooth_out*delta)
-			player.rotation.y = rotation.y
-			if cam.get_cam_pos()[0].distance_to(pos_argument)<0.01:
-				smooth_out=-1.0
-				
-				
-	#CAMERA MODE 1 = TRIPOD
+		if Global.control_mode<4:
+			cam.set_cam_pos(cam_anchor.global_position, cam_anchor.global_rotation)
+		elif Global.control_mode>=4 && Global.control_mode<=5:
+			cam.set_cam_pos(Vector3(get_tree().get_first_node_in_group("Player").global_position.x,1,get_tree().get_first_node_in_group("Player").global_position.z), Vector3(0.,-1.5708+get_tree().get_first_node_in_group("Player").rotation.y,0.))
+	
 	if Global.camera_mode==1:
-		cam.set_cam_pos(pos_argument, rot_argument)
+		cam.set_cam_pos(cam_anchor.global_position, cam_anchor.global_rotation)
